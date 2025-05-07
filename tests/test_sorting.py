023694c7794ff62ac5b3ad972df2665295bf68ef
azrasugec <azrasugec@gmail.com>
2025-05-07 11:46:47 +0300

    update

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,4 +1,4 @@
-import unittest
+iimport unittest
 from sorting_algorithms import *
 from ds import *
 
@@ -11,6 +11,16 @@ class TestSorting(unittest.TestCase):
 
     def test_bubble_sort_linked_list(self):
         self.assertEqual(list(bubble_sort(self.linked_list1)), [1,2,3,4,5,6,7,8,9])
+
+
+     l2 = [4, -3, 0, 2, -1]
+     linked_list2 = convert_to_linked_list(l2)
+
+     def test_bubble_sort_list_with_negatives(self):
+         self.assertEqual(bubble_sort(self.l2), [-3, -1, 0, 2, 4])
+
+     def test_bubble_sort_linked_list_with_negatives(self):
+         self.assertEqual(list(bubble_sort(self.linked_list2)), [-3, -1, 0, 2, 4])
 
 if __name__ == "__main__":
     unittest.main()
